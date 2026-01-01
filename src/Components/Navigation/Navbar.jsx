46467e37e54072d281d96ca620591f1a67a33725
Abhi solanki <abhi85076@gmail.com>
2026-01-01 11:12:27 +0530

    fixed: destroyOnChange  to destroyOnHidden

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -495,7 +495,7 @@ const Navbar = () => {
                 onCancel={closeContactModal}
                 footer={null}
                 centered
-                destroyOnClose
+                destroyOnHidden
             >
                 <div style={{ display: 'grid', gap: 12 }}>
                     <h3 style={{ margin: 0 }}>Get in touch</h3>
