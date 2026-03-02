77005dac1477cca7a9ef56c85f2b68ce716cf8e8
Abhi solanki <abhi85076@gmail.com>
2026-03-02 14:08:48 +0530

    Updated: title

--- src/Components/HomeComponents/InvestorCharters/InvestorCharters.jsx
+++ src/Components/HomeComponents/InvestorCharters/InvestorCharters.jsx
@@ -12,7 +12,7 @@ const InvestorCharters = () => {
         },
         {
             id: 2,
-            title: "Investor Charter for Depositories and Depository Participants - January 2026",
+            title: "Investor Charter for Depositories and Depository Participants - February 2026",
             pdfLink: "/Image/InvestorCharter/INVESTOR_CHARTER_FEBRUARY-2026.pdf",
             image: "https://images.unsplash.com/photo-1723158597314-4760daf1aaae?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
